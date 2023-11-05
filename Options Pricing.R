13e459bbfd973cf1cff42930c0e84979213b1541
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-05 18:59:53 +0500

    Update Options Pricing.R
    Optimised script:
    
    • Reduced excessive lines
    • Renamed variables
    • Combined operations
    • Edited comments

--- Options Pricing.R
+++ Options Pricing.R
@@ -1,67 +1,28 @@
-delta_calculator <- function(x,
-                             strike_price_1,
-                             interest_rate,
-                             time_to_maturity){
+delta.calculator <- function(x, k, r, y){
   
-  # Define tables for calls and puts
-  call_table <- NULL
-  put_table <- NULL
+  call.t <- NULL # Call table
+  put.t <- NULL # Put table
   
-  # For each column
-  for (n in 1:ncol(x)){ security <- x[,n]
+  for (n in 1:ncol(x)){ s <- x[,n] # For each column
+  
+    s.p <- s[nrow(x),] # Extract recent observations
     
-    # Extract recent observations
-    stock_price <- security[nrow(x),]
+    k.p <- k + s.p # Calculate strike price
     
-    # Calculate strike price
-    strike_price <- strike_price_1 + stock_price
+    d.sd <- apply(s, 2, function(col) sd(diff(log(s))[-1,])) # standard deviation
     
-    # Calculate returns
-    rtrns_delta <- diff(log(security))[-1,]
-    
-    # Calculate standard deviation
-    sd_for_delta <- apply(rtrns_delta, 2, function(col) sd(rtrns_delta))
-    
-    # Calculate first and second deltas  
-    delta_1 <- (log(stock_price/strike_price) +
-                  (interest_rate + ((sd_for_delta^2)/2)) * time_to_maturity) /
-      (sd_for_delta * (time_to_maturity)^0.5)
-    
-    delta_2 <- delta_1 - (sd_for_delta * (time_to_maturity^0.5))
-    
-    # Calculate option call & put prices
-    option_price_call <- stock_price * pnorm(delta_1) - strike_price *
-      exp(-interest_rate * time_to_maturity) * pnorm(delta_2)
-    
-    option_price_put <- (strike_price*exp(-interest_rate*time_to_maturity)) *
-        pnorm(-delta_2) - (stock_price * pnorm(-delta_1))
-    
-    # Round to 2 decimals
-    option_price_call <- round(option_price_call, 2)
-    option_price_put <- round(option_price_put, 2)
+    d.1 <- (log(s.p / k.p) + (r + d.sd ^ 2/2) * y) / (d.sd * y ^ .5)
+    d.2 <- d.1 - d.sd * y ^ .5 # Calculate first and second deltas
     
     # Add call and put values to table 
-    call_table <- rbind(call_table, option_price_call)
-    put_table <- rbind(put_table, option_price_put) }
-  
-  # Format both tables as matrices
-  call_table <- as.matrix(call_table)
-  put_table <- as.matrix(put_table)
-  
-  # Merge these tables
-  final_table_for_options <- data.frame(call_table, put_table)
-  
-  # Assign column names
-  colnames(final_table_for_options) <- c("Call", "Put")
-  
-  # Assign row names
-  rownames(final_table_for_options) <- colnames(x)
-  
-  # Display values
-  final_table_for_options
+    call.t <- rbind(call.t, round(s.p*pnorm(d.1) - k.p*exp(-r*y)*pnorm(d.2),2))
+    put.t <- rbind(put.t, round(k.p*exp(-r*y)*pnorm(-d.2) - s.p*pnorm(-d.1),2)) }
+    
+    options.t <- data.frame(as.matrix(call.t), as.matrix(put.t)) # Merge tables
+    
+    colnames(options.t) <- c("Call", "Put") # Assign column names
+    rownames(options.t) <- colnames(x) # Assign row names
+    
+    options.t # Display values
 }
-# Test 
-delta_calculator(stock_data, 
-                 strike_price_1 = 3,
-                 interest_rate = 0.055,
-                 time_to_maturity = 1)
+delta.calculator(stock_data, k = 3,r = 0.055, y = 1) # Test
