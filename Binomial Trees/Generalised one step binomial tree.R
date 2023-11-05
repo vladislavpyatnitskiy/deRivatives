a62c021f5b6873d0286ec3cf0f68d3de0ea0bf6f
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-05 21:23:36 +0500

    Update Generalised one step binomial tree.R
    Optimised script:
    
    • Reduced excessive lines
    • Combined operations
    • Edited comments
    • Renamed variables

--- Binomial Trees/Generalised one step binomial tree.R
+++ Binomial Trees/Generalised one step binomial tree.R
@@ -1,34 +1,8 @@
 # Function to calculate option value via generalisation
-gen_one_step_bin_tree <- function(time_to_maturity,
-                                   orig_stock_price,
-                                   delta_for_stock,
-                                   interest_rate){
+gen.one.step.bin.tree <- function(y, s, d, r){
   
-  # Return when stock goes up
-  stock_price_up <- (orig_stock_price + delta_for_stock) / orig_stock_price
+  bin <- (exp(r * y) - (s - d) / s) / (2 * d / s) # Calculate probability
   
-  # Return when stock goes down
-  stock_price_down <- (orig_stock_price - delta_for_stock) / orig_stock_price
-  
-  # Calculate probability
-  bin_pr <- (exp(interest_rate * time_to_maturity) - stock_price_down) /
-    (stock_price_up - stock_price_down)
-  
-  # Actual option price
-  bin_option_price <- exp(-interest_rate * time_to_maturity) *
-    (1 * bin_pr + 0 * (1 - bin_pr))
-  
-  # Round
-  bin_option_price <- round(bin_option_price, 3)
-  
-  # Put value in text
-  bin_display <- sprintf("The value of the option today is %s",
-                         bin_option_price)
-  # Display
-  return(bin_display)
+  sprintf("Option Value today is %s", round(exp(-r*y) * (bin + 0*(1 - bin)),3))
 }
-# Test
-gen_one_step_bin_tree(time_to_maturity = 0.25,
-                       orig_stock_price = 20,
-                       delta_for_stock = 2,
-                       interest_rate = 0.12)
+gen.one.step.bin.tree(y = .25, s = 20, d = 2, r = .12) # Test
