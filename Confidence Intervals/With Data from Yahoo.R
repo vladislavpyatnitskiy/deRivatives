20cd97fb485cfb0f493f243e18aca60d21cdb8bb
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-05 20:12:16 +0500

    Update With Data from Yahoo.R
    Optimised script:
    
    • Reduced excessive lines
    • Combined operations
    • Edited comments
    • Renamed variables

--- Confidence Intervals/With Data from Yahoo.R
+++ Confidence Intervals/With Data from Yahoo.R
@@ -1,112 +1,60 @@
 # Calculate Lower & Upper Bounds for stocks
-confidence_intrvl <- function(tickers, time_to_maturity = NULL,
-                              start_date = NULL, end_date = NULL,
-                              interval_values = c(0.025, 0.975)){
+prices.confidence.intervals <- function(tickers, y = NULL, s = NULL, e = NULL,
+                              i.values = c(0.025, 0.975)){
   
-  # Set variable to contain tickers
-  portfolioPrices <- NULL
+  p <- NULL # Set variable to contain tickers
   
   # Loop for data extraction
-  for (Ticker in tickers){
-    if (is.null(start_date) && is.null(end_date) && is.null(time_to_maturity)){
+  for (A in tickers){ if (is.null(s) && is.null(e) && is.null(y)){
       
       # When both start date and end date are defined
-      portfolioPrices <- cbind(portfolioPrices,
-                               getSymbols(Ticker,
-                                          from = start_date,
-                                          to = end_date,
-                                          src = "yahoo", 
-                                          auto.assign=FALSE)[,4]) 
-      # When time to maturity is typed
-      } else if (is.null(start_date) && is.null(end_date)) {
-                                                          
-      portfolioPrices <- cbind(portfolioPrices,
-                               getSymbols(Ticker,
-                                          from = as.Date(Sys.Date()) -
-                                            round(time_to_maturity * 365),
-                                          to = Sys.Date(),
-                                          src = "yahoo", 
-                                          auto.assign=FALSE)[,4]) 
-      # When dates are typed
-      } else if (is.null(time_to_maturity)) {
+      p <- cbind(p,getSymbols(A,from=s,to=e,src="yahoo",auto.assign=F)[,4]) 
+      
+    } else if (is.null(s) && is.null(e)) { # When time to maturity is typed
+      
+      p <- cbind(p,getSymbols(A,from=as.Date(Sys.Date()) - round(y*365), 
+                              to=Sys.Date(), src="yahoo", auto.assign=F)[,4]) 
+      
+    } else if (is.null(y)) { # When both start date and end date are defined
+      
+      p<-cbind(p,getSymbols(A,from=s,to=e,src="yahoo",
+                            auto.assign=F)[,4]) } else {
                                             
-      # When both start date and end date are defined
-      portfolioPrices <- cbind(portfolioPrices,
-                               getSymbols(Ticker,
-                                          from = start_date,
-                                          to = end_date,
-                                          src = "yahoo", 
-                                          auto.assign=FALSE)[,4]) } else {
-      
-      # When dates and time maturity are typed
-      print("Please choose either dates or time to maturity.")
-                                            break } } 
+    # When dates and time maturity are typed
+    print("Please choose either dates or time to maturity.")
+    break } } 
   
-  # Get rid of NAs
-  portfolioPrices <- portfolioPrices[apply(portfolioPrices,1,
-                                           function(x) all(!is.na(x))),]
-  # Put the tickers in data set
-  colnames(portfolioPrices) <- tickers
+  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
+
+  colnames(p) <- tickers # Put the tickers in data set
   
-  # Make data discrete
-  stock_price <- ROC(portfolioPrices, type = "discrete")
+  s.p <- as.timeSeries(p) # Make it time series
   
-  # Make it time series
-  stock_price <- as.timeSeries(portfolioPrices)
+  v <- apply(s.p, 2, function(col) sd(diff(log(s.p))[-1,]))*252^.5 # Volatility
   
-  # Log returns
-  volatility <- diff(log(stock_price))[-1,]
+  p.ci <- s.p[nrow(s.p),] # Select last price
   
-  # Calculate volatility
-  volatility <- apply(volatility, 2, function(col) sd(col)) * (252 ^ 0.5) 
+  r <- as.matrix(s.p[nrow(s.p),]) / as.matrix(s.p[1,]) - 1 # Expected Return
   
-  # Select last price
-  price_ci <- stock_price[nrow(stock_price),]
+  b.array <- NULL # Create empty variable for lower & upper bounds values
   
-  # Calculate expected return
-  expected_return <- as.matrix(stock_price[nrow(stock_price),]) /
-    as.matrix(stock_price[1,]) - 1
-  
-  # Create empty variable to contain values for lower & upper bounds
-  array_bound <- NULL
-  
-  # Create empty variable to contain values for lower & upper bounds
-  for (m in 1:length(interval_values)){
+  # Prices for Lower & Upper bounds
+  for (m in 1:length(i.values)){ b.list <- NULL
     
-    # Create empty variable to contain values for lower & upper bounds
-    bound_values_list <- NULL
+    for (n in 1:ncol(s.p)){ mean.ci <- log(p.ci[,n])+(r[,n]-v[n]^2/2)*y
+      
+      b.list <-rbind(b.list, exp(mean.ci+qnorm(i.values[m])*(v[n]^2*y)^.5)) } 
     
-    # For each column 
-    for (n in 1:ncol(stock_price)){
-      
-      # Calculate mean
-      mean_for_ci <- log(price_ci[,n]) + 
-        (expected_return[,n] - (((volatility[n]) ^ 2) / 2)) * time_to_maturity
-      
-      # Calculate sd
-      sd_for_ci <- ((volatility[n]) ^ 2 * time_to_maturity) ^ 0.5
-      
-      # Calculate value for bound
-      bound_values <- exp(mean_for_ci + qnorm(interval_values[m]) * sd_for_ci)
-      
-      # Add bound value to data frame
-      bound_values_list <-rbind(bound_values_list, bound_values) }
-    
-    # Lower Bound & Upper Bound
-    array_bound <- cbind(array_bound, bound_values_list) }
+    b.array <- cbind(b.array, b.list)} # Lower & Upper Bounds
   
-  # Give column names
-  colnames(array_bound) <- c("Lower Bound", "Upper Bound")
+  colnames(b.array) <- c("Lower Bound", "Upper Bound") # Give column names
   
-  # Make it matrix
-  array_bound <- as.matrix(array_bound)
+  b.array <- as.matrix(b.array) # Make it matrix
   
-  # Give row names
-  rownames(array_bound) <- tickers
+  rownames(b.array) <- tickers # Give row names
   
-  # Display text
-  return(array_bound)
+  return(b.array) # Display text
 }
 # Test
-confidence_intrvl(tickers = c("XOM", "CVX", "SHEL"), time_to_maturity = 0.25,
-                  interval_values = c(0.025, 0.975))
+prices.confidence.intervals(tickers = c("XOM","CVX","SHEL"),y=.25,
+                            i.values=c(.025,.975))
