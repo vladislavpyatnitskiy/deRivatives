128fbbf22104ab68aa1ff7ef979f70dc1ab6c732
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-01-21 10:37:27 +0500

    Update Sample.R
    Renamed variables and reduced blank lines

--- Confidence Intervals/Sample.R
+++ Confidence Intervals/Sample.R
@@ -1,15 +1,12 @@
 # Function to calculate confidence intervals to stock volatility
-confidence_interval <- function(p, r, v, y, rg = 95){
+confidence_interval <- function(p, r, v, y, R = 95){
   
-  mean.ci <- log(p) + (r - v ^ 2 / 2) * y # Mean
+  MEAN <- log(p) + (r - v ^ 2 / 2) * y # Mean
+  SD <- (v ^ 2 * y) ^ .5 # Standard Deviation
   
-  sd.ci <- (v ^ 2 * y) ^ .5 # Standard Deviation
+  L <- exp(MEAN + qnorm((100 - R) / 200) * SD) # Lower Bound
+  U <- exp(MEAN + qnorm(R * .01 + (100 - R) / 200) * SD) # Upper Bound
   
-  lower_bound <- exp(mean.ci + qnorm((100 - rg) / 200) * sd.ci) # Lower Bound
-  
-  upper_bound <- exp(mean.ci+qnorm(rg*.01 + (100-rg)/200)*sd.ci) # Upper Bound
-  
-  sprintf("%s%% probability stock price will be between %.2f and %.2f.",
-          rg, lower_bound, upper_bound) # Values in text
+  sprintf("%s%% probability stock price will be between %.2f and %.2f.",R,L,U) 
 }
 confidence_interval(p = 40, r = .16, v = .2, y = .5, 99) # Test
