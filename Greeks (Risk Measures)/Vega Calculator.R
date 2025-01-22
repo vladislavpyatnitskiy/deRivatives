d0ffd7c8779f7e4e75923f014eebc2c5ec713295
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-01-22 11:36:42 +0500

    Update Vega Calculator.R
    Optimised script:
    
    • Rewrote equations
    • Reduced excessive lines

--- Greeks (Risk Measures)/Vega Calculator.R
+++ Greeks (Risk Measures)/Vega Calculator.R
@@ -1,13 +1,7 @@
-vega.calculator <- function(S, K, v, y, r){ # Vega Option
+vega.calculator <- function(S, K, v, y, r){ # Vega
   
-  d1 <- (log(S / K) + (r + v ^ 2/2) * y) / v / y ^ .5 # Delta 1
+  d <- (log(S / K) + (r + v ^ 2 / 2) * y) / v / y ^ .5 # Delta
   
-  d2 <- d1 - v * y ^ .5 # Delta 2
-  
-  d.d1 <- exp(-d1 ^ 2 / 2) / (2 * pi) ^ .5 # Derived Delta 1
-  
-  vega.parameter <- S * y ^ .5 * d.d1 # Vega value
-  
-  vega.parameter # Display value
+  S * y ^ .5 * exp(-d ^ 2 / 2) / (2 * pi) ^ .5 # Vega value
 }
 vega.calculator(49, 50, .2, .3846, .05) # Test
