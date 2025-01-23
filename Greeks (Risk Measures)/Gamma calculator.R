bbd2a3012946ad387f937792247acd201c284e44
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-01-23 11:59:01 +0500

    Update Gamma calculator.R
    Optimised script:
    
    • Rewrote equations
    • Reduced excessive lines

--- Greeks (Risk Measures)/Gamma calculator.R
+++ Greeks (Risk Measures)/Gamma calculator.R
@@ -1,15 +1,7 @@
 gamma.calculator <- function(S, K, v, y, r){ # Gamma
   
-  d1 <- (log(S / K) + (r + v ^ 2/2) * y) / v / y ^ .5 # Delta 1
+  d <- (log(S / K) + (r + v ^ 2 / 2) * y) / v / y ^ .5 # Delta
   
-  d2 <- d1 - v * y ^ .5 # Delta 2
-  
-  d.d1 <- exp(-d1 ^ 2 / 2) / (2 * pi) ^ .5 # Derived Delta 1
-  
-  gamma.parameter <- d.d1 / S / v / y ^ .5 # Gamma value
-  
-  names(gamma.parameter) <- "Call & Put" # Name
-  
-  gamma.parameter # Display value
+  exp(-d ^ 2 / 2) / (2 * pi) ^ .5 / S / v / y ^ .5 # Gamma value
 }
 gamma.calculator(49, 50, .2, .3846, .05) # Test
