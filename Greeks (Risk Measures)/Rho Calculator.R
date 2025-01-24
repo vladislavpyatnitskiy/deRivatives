cacfeecdf9e48639117f9a5ee8de038d69a73bbd
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-01-24 12:00:05 +0500

    Update Rho Calculator.R
    Optimised script:
    
    • Rewrote equations
    • Reduced excessive lines

--- Greeks (Risk Measures)/Rho Calculator.R
+++ Greeks (Risk Measures)/Rho Calculator.R
@@ -1,14 +1,11 @@
 rho.calculator <- function(S, K, v, y, r){ # Rho Option
   
-  d1 <- (log(S / K) + (r + v ^ 2/2) * y) / v / y ^ .5 # Delta 1
+  d2 <- (log(S / K) + (r + v ^ 2 / 2) * y) / v / y ^ .5 - v * y ^ .5 # Delta
   
-  d2 <- d1 - v * y ^ .5 # Delta 2
+  R <- c(K * y * exp(-r * y) * pnorm(d2), -K * y * exp(-r * y) * pnorm(-d2))
   
-  # Data Frame with Rho option values
-  rho.df <- data.frame(K*y*exp(-r*y) * pnorm(d2), - K*y*exp(-r*y) * pnorm(-d2))
+  names(R) <- c("Call", "Put") # Rho names
   
-  names(rho.df) <- c("Call", "Put") # Rho names
-  
-  rho.df # Display value
+  R # Display value
 }
 rho.calculator(49, 50, .2, .3846, .05) # Test
