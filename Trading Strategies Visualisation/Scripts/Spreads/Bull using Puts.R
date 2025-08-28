0155e4a6ea8ae045435c661ace76bfa18eda44f6
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-08-28 11:01:57 +0500

    Update Bull using Puts.R
    Optimised script:
    
    • Reduced ELSE statement

--- Trading Strategies Visualisation/Scripts/Spreads/Bull using Puts.R
+++ Trading Strategies Visualisation/Scripts/Spreads/Bull using Puts.R
@@ -1,35 +1,35 @@
 spread.bull.put <- function(k1, k2, c1, c2){ # Bull Spread using Puts
   
-  if (isTRUE(k1 >= k2)){ message("Long put cannot be higher than Short Put") }
+  if (k1 >= k2) return(message("Long put cannot be greater than Short Put"))
   
-  else { # if strike prices are fine, than define long and short puts:
+  # if strike prices are fine, than define long and short puts:
     
-    L <- rbind(cbind(seq(k1 - 5, k1, 1), k1 + c1 - c2 - seq(k1 - 5, k1, 1)),
-               cbind(seq(k1 + 1, k2 + 5, 1), c1 - c2)) # Long Put
+  L <- rbind(cbind(seq(k1 - 5, k1, 1), k1 + c1 - c2 - seq(k1 - 5, k1, 1)),
+             cbind(seq(k1 + 1, k2 + 5, 1), c1 - c2)) # Long Put
     
-    S <- rbind(cbind(seq(k1 - 5, k2 - 1, 1), seq(k1 - 5, k2 - 1, 1) - k1 - 1),
-               cbind(seq(k2, k2 + 5, 1), k2 - k1 - c1)) # Short Put
+  S <- rbind(cbind(seq(k1 - 5, k2 - 1, 1), seq(k1 - 5, k2 - 1, 1) - k1 - 1),
+             cbind(seq(k2, k2 + 5, 1), k2 - k1 - c1)) # Short Put
     
-    colnames(L) <- c("Stock Price", "Profit from Long Put") # Column names         
-    colnames(S) <- c("Stock Price", "Profit from Short Put")       
+  colnames(L) <- c("Stock Price", "Profit from Long Put") # Column names         
+  colnames(S) <- c("Stock Price", "Profit from Short Put")       
     
-    DF <- merge(L, S, by = "Stock Price") # Merge stock and option data frames
+  DF <- merge(L, S, by = "Stock Price") # Merge stock and option data frames
     
-    DF$`Total Payoff` <- DF[,2] + DF[,3] # Calculate total pay-off
+  DF$`Total Payoff` <- DF[,2] + DF[,3] # Calculate total pay-off
     
-    plot(x = DF[,1], y = DF[,4], las = 1, ylab = "Profit ($)", type = "l",
-         xlab = "Stock Price ($)", ylim = c(min(DF[,2:4]), max(DF[,2:4])),
-         main = "P&L from Bull Spread using Puts", lwd = 5, col = "red")
+  plot(x = DF[,1], y = DF[,4], las = 1, ylab = "Profit ($)", type = "l",
+       xlab = "Stock Price ($)", ylim = c(min(DF[,2:4]), max(DF[,2:4])),
+       main = "P&L from Bull Spread using Puts", lwd = 5, col = "red")
     
-    grid(nx = NULL, ny = NULL, col = "grey", lty = 3) # Grid and lines for puts
+  grid(nx = NULL, ny = NULL, col = "grey", lty = 3) # Grid and lines for puts
     
-    for (n in 2:3){ lines(x = DF[,1], y = DF[,n], lwd = 2, lty = 2) }
+  for (n in 2:3){ lines(x = DF[,1], y = DF[,n], lwd = 2, lty = 2) }
     
-    abline(h = 0) # Break Even line
+  abline(h = 0) # Break Even line
     
-    text(x = k2 + 2.5, y = 3.75, sprintf("Short Put, Strike Price of $%s", k2))
-    text(x = k2 + 2.5, y = -1.75, sprintf("Long Put, Strike Price of $%s", k1))
+  text(x = k2 + 2.5, y = 3.75, sprintf("Short Put, Strike Price of $%s", k2))
+  text(x = k2 + 2.5, y = -1.75, sprintf("Long Put, Strike Price of $%s", k1))
     
-    DF } # display data frame
+  DF # display data frame
 }
-spread.bull.put(30, 35, 1, 3) # Test
+spread.bull.put(36, 35, 1, 3) # Test
