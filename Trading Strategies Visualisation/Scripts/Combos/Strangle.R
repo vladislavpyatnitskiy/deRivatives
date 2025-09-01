6e15858a0cc1e64e64e26d9efbf9caac2037485f
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-09-01 11:32:37 +0500

    Update Strangle.R
    Optimised script:
    
    • Reduced ELSE statement
    • Warning message pops up automatically when strike prices are wrong

--- Trading Strategies Visualisation/Scripts/Combos/Strangle.R
+++ Trading Strategies Visualisation/Scripts/Combos/Strangle.R
@@ -1,37 +1,36 @@
 combo.strangle <- function(k1, k2, call, put){ # Strangle Combination
   
-  if (isTRUE(k1 >= k2)){ message("Long Put cannot be higher than Long Call") }
+  if (k1 >= k2) return(message("Long Put cannot be higher than Long Call"))
   
-  else { # if strike prices are fine, let us define calls below:
+  # if strike prices are fine, let us define calls below:
+  C <- rbind(cbind(seq(k1 - 10, k2, 1), -call),
+             cbind(seq(k2 + 1, k2 + 10, 1), seq(10) - call)) # Long Call
     
-    C <- rbind(cbind(seq(k1 - 10, k2, 1), -call),
-               cbind(seq(k2 + 1, k2 + 10, 1), seq(10) - call)) # Long Call
+  P <- rbind(cbind(seq(k1 - 10, k1 - 1, 1), put - seq(-5, 4, 1) - 1),
+             cbind(seq(k1, k2 + 10, 1), -put)) # Long Put
     
-    P <- rbind(cbind(seq(k1 - 10, k1 - 1, 1), put - seq(-5, 4, 1) - 1),
-                cbind(seq(k1, k2 + 10, 1), -put)) # Long Put
+  colnames(C) <- c("Stock Price", "Profit from Long Call") # Column names
+  colnames(P) <- c("Stock Price", "Profit from Long Put")
     
-    colnames(C) <- c("Stock Price", "Profit from Long Call") # Column names
-    colnames(P) <- c("Stock Price", "Profit from Long Put")
+  DF <- merge(C, P, by = "Stock Price") # Merge stock and option data frames
     
-    DF <- merge(C, P, by = "Stock Price") # Merge stock and option data frames
+  DF$`Total Payoff` <- DF[,2] + DF[,3] # Total Pay-off column
     
-    DF$`Total Payoff` <- DF[,2] + DF[,3] # Total Pay-off column
+  plot(x = DF[,1], y = DF[,4], las = 1, ylab = "Profit ($)", type = "l",
+       xlab = "Stock Price ($)", ylim = c(min(DF[,4]), max(DF[,4])),
+       main = "Profit & Loss from Strangle", lwd = 5, col = "red")
     
-    plot(x = DF[,1], y = DF[,4], las = 1, ylab = "Profit ($)", type = "l",
-         xlab = "Stock Price ($)", ylim = c(min(DF[,4]), max(DF[,4])), 
-         main = "Profit & Loss from Strangle", lwd = 5, col = "red")
+  grid(nx = NULL, ny = NULL, col = "grey", lty = 3) # Grid
     
-    grid(nx = NULL, ny = NULL, col = "grey", lty = 3) # Grid
+  abline(h = 0) # Break Even line
     
-    abline(h = 0) # Break Even line
+  for (n in 2:3){ lines(x = DF[,1], y = DF[,n], lwd = 2, lty = 2) } 
     
-    for (n in 2:3){ lines(x = DF[,1], y = DF[,n], lwd = 2, lty = 2) } 
+  text(x = k2 + 7.5, y = 0.25 - put,
+       sprintf("Long Put, Strike Price of $%s", k1)) # Long Put
+  text(x = k1 - 7.5, y = 0.25 - call,
+       sprintf("Long Call, Strike Price of $%s", k2)) # Long Call
     
-    text(x = k2 + 7.5, y = 0.25 - put,
-         sprintf("Long Put, Strike Price of $%s", k1)) # Long Put
-    text(x = k1 - 7.5, y = 0.25 - call,
-         sprintf("Long Call, Strike Price of $%s", k2)) # Long Call
-    
-    DF } # Pay-off
+  DF # Pay-off
 }
 combo.strangle(55, 60, 1, 3) # Test
